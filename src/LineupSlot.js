6a0d91681621f229d741b67f19310557d431743a
Alex-Shane <ashane1230@gmail.com>
2023-08-21 19:32:30 -0400

    adding choice to view pdf in broswer or download it

--- src/LineupSlot.js
+++ src/LineupSlot.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Choice from './Choice.js'
 
 function LineupSlot({ order, linkText, linkTo, position}) {
+
+    const [showChoice, setShowChoice] = useState(false);
+
+    const handlePdfLinkClick = () => {
+        setShowChoice(true);
+      };
 
   return (
     <div className="lineup-slot">
       <div className="slot-number">{order}</div>
       <div className="slot-link">
-        <a href={linkTo} target={linkText.includes('.pdf') ? '_blank' : '_self'} rel="noopener noreferrer">
+        <a href={linkTo} target={linkTo.includes('.pdf') ? '_blank' : '_self'} download ={linkTo.includes('.pdf') ? "Alex Shane Resume" : null} rel="noopener noreferrer"
+           onClick={(e) => {
+            e.preventDefault();
+            handlePdfLinkClick();
+          }}
+          >
           {linkText}
         </a>
       </div>
       <div className="slot-position">{position}</div>
+      {showChoice && <Choice path = {linkTo} type = {linkText} setShowChoice={setShowChoice} />}
     </div>
   );
 }
